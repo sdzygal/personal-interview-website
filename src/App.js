c5719dc448ad56ef6ab3c82e2085dd6268c3cb4d
szdygal <sdzygal@gmail.com>
2022-07-07 21:34:21 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { MultiStepProgressBar } from "./components/MultiStepProgressBar";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { MultiStepForm } from "./components/MultiStepForm";
+import { questions } from "./Questions";
+import { NavBar } from './Components/NavBar';
+import { Banner } from './Components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
     </div>
   );
 }
